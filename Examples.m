d4cfb444cd93e0462dfea2dee93efec8d655fc23
AlvaroGohe <alvaro_gohe@usal.es>
2024-09-03 12:39:24 +0100

    Fixed some issues

--- Examples.m
+++ Examples.m
@@ -13,7 +13,7 @@ J5 := Jacobian(C5);
 Kum5 := GeneralKummerSurface(J5);
 Jac5 := GeneralJacobianSurface(J5);
 
-// Example in characteristic zero
+// Example 1 in characteristic zero
 Q0<r> := ext<Rationals() | Polynomial([-1,-1,1])>;
 PolQ0<x> := PolynomialRing(Q0);
 f0 := -(20*r*x + 5*r^2*x - 20*x^2 - 39*r*x^2 - 7*r^2*x^2 + 34*x^3 + 24*r*x^3 + 2*r^2*x^3 - 17*x^4 - 6*r*x^4 + 4*x^5 + r*x^5);
@@ -23,6 +23,20 @@ J0 := Jacobian(C0);
 Kum0 := GeneralKummerSurface(J0);
 Jac0 := GeneralJacobianSurface(J0);
 Des0 := DesingularisedKummer(J0);
+Ex0, Tr0, Des0 := Lines(J0, Des0);
+W0 := WeddleSurface(J0);
+
+// Example 2 in characteristic zero
+PolQA0<x> := PolynomialRing(Rationals());
+fA0 := 1 - 4*x + 2*x^2 - x^3 + x^4;
+gA0 := x*(x-1)^2;
+CA0 := HyperellipticCurve(fA0,gA0);
+JA0 := Jacobian(CA0);
+KumA0 := GeneralKummerSurface(JA0);
+JacA0 := GeneralJacobianSurface(JA0);
+DesA0 := DesingularisedKummer(JA0);
+ExA0, TrA0, DesA0 := Lines(JA0, DesA0);
+WA0 := WeddleSurface(JA0);
 
 // Example of a supersingular genus 2 curve in characteristic 2
 QSS<r> := GF(4);
@@ -34,17 +48,24 @@ JSS := Jacobian(CSS);
 KumSS := GeneralKummerSurface(JSS);
 JacSS := GeneralJacobianSurface(JSS);
 DesSS := DesingularisedKummer(JSS);
+ExSS, TrSS, DesSS := Lines(JSS, DesSS);
+WSS := WeddleSurface(JSS);
 
 // Example of an almost ordinary genus 2 curve in characteristic 2
-QAO<r> := GF(8);
+QAO<r> := GF(2);
 PolQAO<x> := PolynomialRing(QAO);
-fAO := x^6+x^5+r*x+1;
-gAO := (x+r)^2*(x+r^2);
+fAO := x^6+x^5+1;
+gAO := x*(x-1)^2;
 CAO := HyperellipticCurve(fAO,gAO);
 JAO := Jacobian(CAO);
 KumAO := GeneralKummerSurface(JAO);
 JacAO := GeneralJacobianSurface(JAO);
 DesAO := DesingularisedKummer(JAO);
+ExAO, TrAO, plAO := Lines(JAO, DesAO);
+WAO := WeddleSurface(JAO);
+phiAO := WeddleQuotient(JAO, DesAO, WAO);
+WsptsAO := SingularPoints(WAO);
+[TjurinaP3(pt): pt in WsptsAO];
 
 // Example of an ordinary genus 2 curve in characteristic 2. This can be found by specialising the characteristic zero example
 QO<r> := GF(4);
@@ -56,9 +77,16 @@ JO := Jacobian(CO);
 KumO := GeneralKummerSurface(JO);
 JacO := GeneralJacobianSurface(JO);
 DesO := DesingularisedKummer(JO);
+ExO, TrO, DesO := Lines(JO, DesO);
+WO := WeddleSurface(JO);
+phiO := WeddleQuotient(JO, DesO, WO);
+WsptsO := SingularPoints(WO);
+Factorisation(gO);
+[TjurinaP3(pt): pt in WsptsO];
+
 
 // A second example of an ordinary genus 2 curve in characteristic 2
-QO2<r> := GF(2);
+QO2<r> := GF(4);
 PolQO2<x> := PolynomialRing(QO2);
 fO2 := x^6+x^5+1;
 gO2 := x*(x^2+x+1);
@@ -70,7 +98,7 @@ DesO2 := DesingularisedKummer(JO2);
 
 // The example whose associated Kummer have everywhere good reduction
 PolQ<x> := PolynomialRing(Rationals());
-QDK<w> := NumberField(x^2-x-105);
+QDK<w> := NumberField(x^2-x-88);
 PolQDK<x> := PolynomialRing(QDK);
 gDK := (w + 1)*x^3 + x^2 + w*x + w + 1;
 fDK := -(15*w + 149)*x^6 - (1119*w + 9948)*x^5 - (36545*w + 325090)*x^4 -(636332*w + 5659370)*x^3 - (6227174*w + 55387985)*x^2 -(32480001*w + 288869715)*x - 70532813*w - 627353458;
@@ -80,17 +108,3 @@ KumDK := GeneralKummerSurface(JDK);
 JacDK := GeneralJacobianSurface(JDK);
 DesDK := DesingularisedKummer(JDK);
 
-QDK2 := GF(8);
-PolQDK2<x> := PolynomialRing(QDK2);
-w := 0;
-gDK2 := (w + 1)*x^3 + x^2 + w*x + w + 1;
-fDK2 := -(15*w + 149)*x^6 - (1119*w + 9948)*x^5 - (36545*w + 325090)*x^4 -(636332*w + 5659370)*x^3 - (6227174*w + 55387985)*x^2 -(32480001*w + 288869715)*x - 70532813*w - 627353458;
-CDK2 := HyperellipticCurve(fDK2,gDK2);
-JDK2 := Jacobian(CDK2);
-KumDK2 := GeneralKummerSurface(JDK2);
-JacDK2 := GeneralJacobianSurface(JDK2);
-DesDK2 := DesingularisedKummer(JDK2);
-
- 
-
- 
